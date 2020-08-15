2900d767ad8f3db6c4f5670fbd7e021a74af4d84
Angela Du <angeladu2002@gmail.com>
2020-08-14 20:21:31 -0400

    Init Project

---
+++ TecHacks2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TecHacks2020
+//
+//  Created by Angela Du on 2020-08-14.
+//  Copyright © 2020 Angela Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
