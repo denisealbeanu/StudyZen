8e4257e7731abcd70812ab98a0ddc5fb26495176
Denise Albeanu <63042788+denisealbeanu@users.noreply.github.com>
2020-08-14 20:05:32 -0400

    Add files via upload

---
+++ MyCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalendar
+//
+//  Created by Denise Albeanu on 2020-08-14.
+//  Copyright © 2020 Denise Albeanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
