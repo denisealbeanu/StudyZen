89fcaadab4d9ac5e86f78a60cf50548a2516389d
Denise Albeanu <63042788+denisealbeanu@users.noreply.github.com>
2020-08-14 23:18:59 -0400

    c
    c

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TecHacks2020' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TecHacks2020
+
+pod 'FSCalendar'
+
+
+end
--- TecHacks2020/ViewController.swift
+++ TecHacks2020/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    var time = 0
+    // timer
+    var timer = Timer()
+    
+    @IBAction func start(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        timer.invalidate()
+        time = 0
+    }
+    @objc func action()
+    {
+    time += 1
+    label.text = String(time)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
     }
 
 
