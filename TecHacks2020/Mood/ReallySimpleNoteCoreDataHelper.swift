845e7429df64dc2a0464b08da09b3b88e6b2bf00
Angela Du <narutorocks93@gmail.com>
2020-08-16 07:23:34 -0400

    Revert "Revert "Merge remote-tracking branch 'refs/remotes/origin/master'""
    This reverts commit bb885b5b9897fa2688b6e1515cef6bc6c993258b.

--- TecHacks2020/Mood/ReallySimpleNoteCoreDataHelper.swift
+++ TecHacks2020/Mood/ReallySimpleNoteCoreDataHelper.swift
@@ -1,10 +1,4 @@
-//
-//  ReallySimpleNoteCoreDataHelper.swift
-//  rsnt-really-simple-note-taking-ios
-//
-//  Created by Németh László Harri on 2019. 01. 25..
-//  Copyright © 2019. Németh László Harri. All rights reserved.
-//
+
 
 import Foundation
 import CoreData
